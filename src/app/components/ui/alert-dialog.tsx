ca1ffd4af53b316d71f62a1f470930a8e8aa38b5
kkavya007 <kavarthavarapukavya@gmail.com>
2026-07-24 17:12:43 +0530

    changes in alert popup

--- src/app/components/ui/alert-dialog.tsx
+++ src/app/components/ui/alert-dialog.tsx
@@ -136,7 +136,10 @@ function AlertDialogCancel({
 }: React.ComponentProps<typeof AlertDialogPrimitive.Cancel>) {
   return (
     <AlertDialogPrimitive.Cancel
-      className={cn(buttonVariants({ variant: "outline" }), className)}
+      className={cn(
+        buttonVariants({ variant: "outline" }),
+        "hover:bg-transparent hover:text-foreground",
+        className,)}
       {...props}
     />
   );
